6ece6a59a196c5a5174fefd876f28b7953f48511
Anchal Gurung <anchalghale@gmail.com>
2022-03-02 15:03:19 +0545

    Adds basic css

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <div className="header">LolAnalytics</div>
+      <div className="features">
+        <div className="feature-item">
+          <div className="feature-header">Beta version releasing soon</div>
+          <div className="feature-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus turpis sem,
+            interdum volutpat enim tristique non. Sed et nulla non libero sodales consectetur.
+            Mauris euismod nibh finibus ligula finibus lobortis
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
