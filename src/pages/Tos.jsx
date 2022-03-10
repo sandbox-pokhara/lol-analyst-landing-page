cf341a646a3b6d17445bae9e9239406f0fe1087f
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2022-03-10 18:16:10 +0545

    Adds className to all tags in Tos and PrivacyPolicy

--- src/pages/Tos.jsx
+++ src/pages/Tos.jsx
@@ -4,21 +4,23 @@ import { Link } from "react-router-dom";
 export default function Tos() {
   return (
     <div className="privacy-policy">
-      <h1>Terms and Conditions</h1>
-      <p>Last updated: March 09, 2022</p>
-      <p>Please read these terms and conditions carefully before using Our Service.</p>
-      <h1>Interpretation and Definitions</h1>
-      <h2>Interpretation</h2>
-      <p>
+      <h1 className="heading-large">Terms and Conditions</h1>
+      <p className="paragraph">Last updated: March 09, 2022</p>
+      <p className="paragraph">
+        Please read these terms and conditions carefully before using Our Service.
+      </p>
+      <h1 className="heading-large">Interpretation and Definitions</h1>
+      <h2 className="heading">Interpretation</h2>
+      <p className="paragraph">
         The words of which the initial letter is capitalized have meanings defined under the
         following conditions. The following definitions shall have the same meaning regardless of
         whether they appear in singular or in plural.
       </p>
-      <h2>Definitions</h2>
-      <p>For the purposes of these Terms and Conditions:</p>
-      <ul>
+      <h2 className="heading">Definitions</h2>
+      <p className="paragraph">For the purposes of these Terms and Conditions:</p>
+      <ul className="paragraph">
         <li>
-          <p>
+          <p className="paragraph">
             <strong>Affiliate</strong> means an entity that controls, is controlled by or is under
             common control with a party, where &quot;control&quot; means ownership of 50% or more
             of the shares, equity interest or other securities entitled to vote for election of
@@ -26,44 +28,44 @@ export default function Tos() {
           </p>
         </li>
         <li>
-          <p>
+          <p className="paragraph">
             <strong>Country</strong> refers to: Nepal
           </p>
         </li>
         <li>
-          <p>
+          <p className="paragraph">
             <strong>Company</strong> (referred to as either &quot;the Company&quot;,
             &quot;We&quot;, &quot;Us&quot; or &quot;Our&quot; in this Agreement) refers to Sandbox
             Software Pvt. Ltd., Pokhara, Nepal.
           </p>
         </li>
         <li>
-          <p>
+          <p className="paragraph">
             <strong>Device</strong> means any device that can access the Service such as a
             computer, a cellphone or a digital tablet.
           </p>
         </li>
         <li>
-          <p>
+          <p className="paragraph">
             <strong>Service</strong> refers to the Website.
           </p>
         </li>
         <li>
-          <p>
+          <p className="paragraph">
             <strong>Terms and Conditions</strong> (also referred as &quot;Terms&quot;) mean these
             Terms and Conditions that form the entire agreement between You and the Company
             regarding the use of the Service.
           </p>
         </li>
         <li>
-          <p>
+          <p className="paragraph">
             <strong>Third-party Social Media Service</strong> means any services or content
             (including data, information, products or services) provided by a third-party that may
             be displayed, included or made available by the Service.
           </p>
         </li>
         <li>
-          <p>
+          <p className="paragraph">
             <strong>Website</strong> refers to LolAnalystApp, accessible from{" "}
             <a
               href="https://lolanalyst.app"
@@ -75,42 +77,42 @@ export default function Tos() {
           </p>
         </li>
         <li>
-          <p>
+          <p className="paragraph">
             <strong>You</strong> means the individual accessing or using the Service, or the
             company, or other legal entity on behalf of which such individual is accessing or using
             the Service, as applicable.
           </p>
         </li>
       </ul>
-      <h1>Acknowledgment</h1>
-      <p>
+      <h1 className="heading-large">Acknowledgment</h1>
+      <p className="paragraph">
         These are the Terms and Conditions governing the use of this Service and the agreement that
         operates between You and the Company. These Terms and Conditions set out the rights and
         obligations of all users regarding the use of the Service.
       </p>
-      <p>
+      <p className="paragraph">
         Your access to and use of the Service is conditioned on Your acceptance of and compliance
         with these Terms and Conditions. These Terms and Conditions apply to all visitors, users
         and others who access or use the Service.
       </p>
-      <p>
+      <p className="paragraph">
         By accessing or using the Service You agree to be bound by these Terms and Conditions. If
         You disagree with any part of these Terms and Conditions then You may not access the
         Service.
       </p>
-      <p>
+      <p className="paragraph">
         Your access to and use of the Service is also conditioned on Your acceptance of and
         compliance with the Privacy Policy of the Company. Our Privacy Policy describes Our
         policies and procedures on the collection, use and disclosure of Your personal information
         when You use the Application or the Website and tells You about Your privacy rights and how
         the law protects You. Please read Our Privacy Policy carefully before using Our Service.
       </p>
-      <h1>Links to Other Websites</h1>
-      <p>
+      <h1 className="heading-large">Links to Other Websites</h1>
+      <p className="paragraph">
         Our Service may contain links to third-party web sites or services that are not owned or
         controlled by the Company.
       </p>
-      <p>
+      <p className="paragraph">
         The Company has no control over, and assumes no responsibility for, the content, privacy
         policies, or practices of any third party web sites or services. You further acknowledge
         and agree that the Company shall not be responsible or liable, directly or indirectly, for
@@ -118,19 +120,21 @@ export default function Tos() {
         reliance on any such content, goods or services available on or through any such web sites
         or services.
       </p>
-      <p>
+      <p className="paragraph">
         We strongly advise You to read the terms and conditions and privacy policies of any
         third-party web sites or services that You visit.
       </p>
-      <h1>Termination</h1>
-      <p>
+      <h1 className="heading-large">Termination</h1>
+      <p className="paragraph">
         We may terminate or suspend Your access immediately, without prior notice or liability, for
         any reason whatsoever, including without limitation if You breach these Terms and
         Conditions.
       </p>
-      <p>Upon termination, Your right to use the Service will cease immediately.</p>
-      <h1>&quot;AS IS&quot; and &quot;AS AVAILABLE&quot; Disclaimer</h1>
-      <p>
+      <p className="paragraph">
+        Upon termination, Your right to use the Service will cease immediately.
+      </p>
+      <h1 className="heading-large">&quot;AS IS&quot; and &quot;AS AVAILABLE&quot; Disclaimer</h1>
+      <p className="paragraph">
         The Service is provided to You &quot;AS IS&quot; and &quot;AS AVAILABLE&quot; and with all
         faults and defects without warranty of any kind. To the maximum extent permitted under
         applicable law, the Company, on its own behalf and on behalf of its Affiliates and its and
@@ -145,7 +149,7 @@ export default function Tos() {
         meet any performance or reliability standards or be error free or that any errors or
         defects can or will be corrected.
       </p>
-      <p>
+      <p className="paragraph">
         Without limiting the foregoing, neither the Company nor any of the company's provider makes
         any representation or warranty of any kind, express or implied: (i) as to the operation or
         availability of the Service, or the information, content, and materials or products
@@ -155,28 +159,30 @@ export default function Tos() {
         behalf of the Company are free of viruses, scripts, trojan horses, worms, malware,
         timebombs or other harmful components.
       </p>
-      <p>
+      <p className="paragraph">
         Some jurisdictions do not allow the exclusion of certain types of warranties or limitations
         on applicable statutory rights of a consumer, so some or all of the above exclusions and
         limitations may not apply to You. But in such a case the exclusions and limitations set
         forth in this section shall be applied to the greatest extent enforceable under applicable
         law.
       </p>
-      <h1>Changes to These Terms and Conditions</h1>
-      <p>
+      <h1 className="heading-large">Changes to These Terms and Conditions</h1>
+      <p className="paragraph">
         We reserve the right, at Our sole discretion, to modify or replace these Terms at any time.
         If a revision is material We will make reasonable efforts to provide at least 30 days'
         notice prior to any new terms taking effect. What constitutes a material change will be
         determined at Our sole discretion.
       </p>
-      <p>
+      <p className="paragraph">
         By continuing to access or use Our Service after those revisions become effective, You
         agree to be bound by the revised terms. If You do not agree to the new terms, in whole or
         in part, please stop using the website and the Service.
       </p>
-      <h1>Contact Us</h1>
-      <p>If you have any questions about this Privacy Policy, You can contact us:</p>
-      <ul>
+      <h1 className="heading-large">Contact Us</h1>
+      <p className="paragraph">
+        If you have any questions about this Privacy Policy, You can contact us:
+      </p>
+      <ul className="paragraph">
         <li>
           {"By visiting this page on our website: "}
           <Link to={"/contact-us"}>https://lolanalyst.app/contact-us</Link>
